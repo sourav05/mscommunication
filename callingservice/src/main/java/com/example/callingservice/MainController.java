da2fbd77e6dc396dadf9536024d0d1a8cd96b4fe
sourav05 <40533228+sourav05@users.noreply.github.com>
2018-12-18 12:24:53 +0530

    Second Commit

--- callingservice/src/main/java/com/example/callingservice/MainController.java
+++ callingservice/src/main/java/com/example/callingservice/MainController.java
@@ -11,40 +11,43 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MainController {
 
+	/*@Autowired
+	private DiscoveryClient discoveryClient;*/
+	
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private RestTemplate template;
 	
 	@GetMapping("add/{num1}/{num2}")
 	public String add(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
 		RestTemplate template = new RestTemplate();
-		System.out.println("DISCOVERY_CLIENT ***********  " + this.discoveryClient);
-		System.out.println("this.discoveryClient.getInstances('add-service') = " + this.discoveryClient.getInstances("add-service"));
-		ResponseEntity<String> response = template.getForEntity(this.discoveryClient.getInstances("add-service")
-																	.get(0).getUri() + "/add/" + number1 + "/" + number2, String.class);
+//		System.out.println("DISCOVERY_CLIENT ***********  " + this.discoveryClient);
+//		System.out.println("this.discoveryClient.getInstances('add-service') = " + this.discoveryClient.getInstances("add-service"));
+		ResponseEntity<String> response = template.getForEntity("http://add-service/add/" + number1 + "/" + number2, String.class);
 		return response.getBody();
 	}
 	
 	@GetMapping("sub/{num1}/{num2}")
 	public String substract(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
 		RestTemplate template = new RestTemplate();
-		ResponseEntity<String> response = template.getForEntity(this.discoveryClient.getInstances("SubstractService")
-																	.get(0).getUri() + "/sub/" + number1 + "/" + number2, String.class);
+		/*ResponseEntity<String> response = template.getForEntity(this.discoveryClient.getInstances("SubstractService")
+																	.get(0).getUri() + "/sub/" + number1 + "/" + number2, String.class);*/
+		
+		ResponseEntity<String> response = template.getForEntity("http://sub-service" + "/sub/" + number1 + "/" + number2, String.class);
+		
 		return response.getBody();
 	}
 	
 	@GetMapping("mult/{num1}/{num2}")
 	public String multiply(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
 		RestTemplate template = new RestTemplate();
-		ResponseEntity<String> response = template.getForEntity(this.discoveryClient.getInstances("MultService")
-																	.get(0).getUri() + "/mult/" + number1 + "/" + number2, String.class);
+		ResponseEntity<String> response = template.getForEntity("http://multi-service" + "/mult/" + number1 + "/" + number2, String.class);
 		return response.getBody();
 	}
 	
 	@GetMapping("div/{num1}/{num2}")
 	public String division(@PathVariable(value="num1") int number1, @PathVariable(value="num2") int number2){
 		RestTemplate template = new RestTemplate();
-		ResponseEntity<String> response = template.getForEntity(this.discoveryClient.getInstances("DivService")
-																	.get(0).getUri() + "/div/" + number1 + "/" + number2, String.class);
+		ResponseEntity<String> response = template.getForEntity("http://div-service" + "/div/" + number1 + "/" + number2, String.class);
 		return response.getBody();
 	}
 	
