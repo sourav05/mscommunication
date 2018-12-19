b77b87680c8fe13109898a977435bdcefa21ac28
sourav05 <40533228+sourav05@users.noreply.github.com>
2018-12-19 16:35:41 +0530

    LB_Change

--- callingservice/src/main/java/com/example/callingservice/RibbonConfiguration.java
+++ callingservice/src/main/java/com/example/callingservice/RibbonConfiguration.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
 
 public class RibbonConfiguration {
 
 	@Bean
     public IRule ribbonRule(IClientConfig config) {
-        return new RoundRobinRule();
+        return new WeightedResponseTimeRule();
     }
 }
